e32888694d96a54a5ed37f5f61a5bf8ba83df6e2
Walter Arias <walter.arias.aguirre@gmail.com>
2020-07-14 14:10:01 -0500

    ..

--- sesion7/App/arreglos.php
+++ sesion7/App/arreglos.php
@@ -6,49 +6,57 @@ de arreglos
 //Reordena aleatoriamente una secuencia de elementos dentro de la matriz
 /* $frutas = ['mango','papaya','melon','sandia','mango','toronja','pomelo'];
 shuffle($frutas);
-print_r($frutas);
- */
+echo '<pre>';
+print_r($frutas); */
+
 
  //invierte una secuencia de elementos dentro de la matriz
 /* $frutas = ['mango','papaya','melon','sandia','mango','toronja','pomelo'];
 $nuevo = array_reverse($frutas);
+echo '<pre>';
 print_r($nuevo); */
 
 
 //Extrae elementos dentro de la matriz
 /* $frutas = ['mango','papaya','melon','sandia','mango','toronja','pomelo'];
-$nuevo = array_slice($frutas, 2, 3);
+$nuevo = array_slice($frutas, 4, 2);
+echo '<pre>';
 print_r($nuevo); */
 
 
 //llenado automatico de matriz
-/* $numeros = range(10, 30);
+/* $numeros = range(1, 10);
+echo '<pre>';
 print_r($numeros);
+/*   $numeros = [63,5,87,2,1,17,11,8];
+echo 'el valor mínimo de la matriz es:'.min($numeros).' '.'el valor máximo de la matriz es:'.max($numeros);
+
  */
-/* $numeros = [63,5,87,2,1,17,11,8];
-echo 'el valor mínimo de la matriz es:'.min($numeros).' '.'el valor máximo de la matriz es:'.max($numeros); */
-
-
 //agregar elementos al principio y al final   ==ojo== solo funciona con matrices indexadas
 
 /* $ciudades =['Cali','Pereira','Tulua','Medellin'];
-array_shift($ciudades);
-array_pop($ciudades);
-//print_r($ciudades);
-array_unshift($ciudades, 'Bucaramanga');
+//array_shift($ciudades);
+//array_pop($ciudades);
+/* echo '<pre>';
+print_r($ciudades);
+//array_unshift($ciudades, 'Bucaramanga');
 array_push($ciudades, 'Bogota');
-print_r($ciudades); */
-
-
+echo '<pre>';
+print_r($ciudades);
+ */
+/*
 //Elementos duplicados
-/* $ciudades =['Cali','Pereira','Tulua','Medellin','Pereira','Medellin'];
+$ciudades =['Cali','Pereira','Tulua','Medellin','Pereira','Medellin'];
 $nuevo = array_unique($ciudades);
-print_r($nuevo); */
-
+echo '<pre>';
+print_r($nuevo);
+ */
 
 //busquedas DE VALORES dentro del arreglo
-/* $ciudades =['Cali','Pereira','Tulua','Medellin'];
-echo in_array('Tulua', $ciudades);
+ /* $ciudades =['Cali','Pereira','Tulua','Medellin'];
+ echo '<pre>';
+print_r($ciudades);
+//echo in_array('Tulua', $ciudades);  //true o false   1 0 o NULL
 
 if (in_array('Tulua', $ciudades)) {
     echo 'la ciudad buscada SI existe en el arreglo';
@@ -57,21 +65,22 @@ if (in_array('Tulua', $ciudades)) {
 } */
 
 //busquedas DE INDICES  dentro del arreglo
-/* $colombia = array(
+/*  $colombia = array(
    'Valle' => 'Cali',
    'Risaralda' => 'Pereira',
    'Boyaca' => 'Tunja',
    'Bolivar' => 'Cartagena',
    'Cauca' => 'Popayan'
 );
+echo '<pre>';
+print_r($colombia);
+// echo array_key_exists('Boyaca', $colombia);
 
-echo array_key_exists('Bolivar', $colombia);
-
-if (array_key_exists('Bolivar', $colombia)) {
-    echo 'el indice buscado SI existe en el arreglo';
-} else {
-    echo 'el indice buscado NO existe en el arreglo';
-} */
+ if (array_key_exists('Bolivar', $colombia)) {
+     echo 'el indice buscado SI existe en el arreglo';
+ } else {
+     echo 'el indice buscado NO existe en el arreglo';
+ } */
 
 //ORDENAMIENTO DE MATRICES
 
@@ -80,16 +89,16 @@ sort($numeros);
 echo '<pre>';
 print_r($numeros); */
 
-//ORDENAMIENTO DE MATRICES ASOCIATIVAS
-/* $colombia = array(
+/* //ORDENAMIENTO DE MATRICES ASOCIATIVAS
+ $colombia = array(
     'Valle' => 'Cali',
     'Risaralda' => 'Pereira',
     'Boyaca' => 'Tunja',
     'Bolivar' => 'Cartagena',
     'Cauca' => 'Popayan'
  );
-ksort($colombia);  //por indice o llave
-//asort($colombia);  //por valor
+//ksort($colombia);  //por indice o llave
+asort($colombia);  //por valor
 echo '<pre>';
 print_r($colombia); */
 
@@ -104,7 +113,7 @@ print_r($colores); */
 //COMPARANDO MATRICES
 //encuentra elementos comunes en las matrices
 $pasteles = ['rosa','lila','salmon','azul cielo','blanco'];
-$basicos = ['azul','amarillo','rojo', 'blanco'];/*
+$basicos = ['azul','amarillo','rojo', 'blanco'];
 /* $comunes = array_intersect($pasteles, $basicos);
 echo '<pre>';
 print_r($comunes); */
@@ -115,8 +124,8 @@ print_r($unico); */
  
 
 //tambien puedo hacer
- if ($pasteles == $basicos) {
-     echo 'son iguales';
- } else {
-     echo 'NO son iguales';
- }
+  if ($pasteles == $basicos) {
+      echo 'son iguales';
+  } else {
+      echo 'NO son iguales';
+  }
---
+++ sesion7/App/fechas.php
@@ -0,0 +1,40 @@
+<?php
+/*
+===SELLO CRONOLOGIO O TIMESTAMP DE UNIX ====
+
+el valor de sello cronológico de UNIX para cualquier punto en el tiempo, representa
+la cantidad de segundos que han transcurrido desde la medianoche del 1 de enero de
+1970 y el momento actual. De esa manera, por ejemplo, el punto del tiempo ubicado el 5 de
+enero de 2008 a las 10:15:00 a.m. se representa en formato de sello cronológico de UNIX con
+el formato 1199508300.
+PHP puede convertir automáticamente un valor de fecha en el formato de sello cronológico
+de UNIX con la función mktime() o time(), que acepta argumentos para día, mes, año, hora,
+minuto y segundo, para regresar el sello cronológico correspondiente a ese instante.
+UNIX fue uno de los primeros sistemas operativos del mundo */
+
+//echo mktime();  //depreciada a partir de la version 7
+date_default_timezone_set("America/Bogota");
+
+echo time();
+echo '<br>';
+echo date('Y-m-d');
+echo '<br>';
+echo date("l");
+echo '<br>';
+echo date('Y-m-d h:i:s');
+echo '<br>';
+echo date(DATE_RFC2822);
+echo '<br>';
+echo '<hr>';
+
+//============  FORMATEO GENERAL DE FECHAS  ==============
+
+//$hoy = date("F j, Y, g:i a");
+//$hoy = date("m.d.y");
+//$hoy = date("j, n, Y");
+//$hoy = date("Ymd");
+//$hoy = date('\i\t \i\s \t\h\e jS \d\a\y.');
+//$hoy = date("D M j G:i:s T Y");
+//$hoy = date("H:i:s");
+
+echo $hoy;
